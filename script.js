5c2aeda0e601aa36f6d5cf5db42cb6e02fa4ba31
SamTanton <samt@unmade.com>
2023-08-08 23:47:56 +0100

    JS function and unnecessary code clean up

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 // Generate computer choice
-const getComputerChoice = (userChoice) => {
-  let computerChoice = ''    
-  if (userChoice === `rock`)
-      {computerChoice === 'paper'
-      {return(`paper`)}
-      }
-    if (userChoice === `paper`)
-     {computerChoice === `scissors` 
-     {return(`scissors`)}
-    }
-      if (userChoice === `scissors`) {
-      computerChoice === `rock` 
-      {return(`rock`)}
-    };}
+const getComputerChoice = (userChoice) => {   
+switch (userChoice) {
+  case 'rock': 
+  return 'paper';
+  case 'paper':
+    return 'scissors';
+  default:
+    return 'rock';
+}}
 
 // Game setup
-const playGame = (userChoice) => {
-  const computerChoice = getComputerChoice(userChoice)
-    document.querySelector("#outcome").innerText = `You chose ${userChoice}, I chose ${computerChoice}, I win better luck next time`
-  }
+const playGame = (userChoice) => document.querySelector("#outcome").innerText = `You chose ${userChoice}, I chose ${getComputerChoice(userChoice)}, I win better luck next time`;
